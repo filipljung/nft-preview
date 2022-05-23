c4bd29bc067b43047dd62b348c9907d2f0e30b28
filipljung <96741967+filipljung@users.noreply.github.com>
2022-05-23 13:51:38 +0200

    Add files via upload
    shop

---
+++ store.html/javascript.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+    document.querySelector("#input").addEventListener("keydown", function(e) {
+      if (e.code === "Enter") {
+          console.log("You pressed the enter button!")
+      }
+    });
+  });
+
+  
